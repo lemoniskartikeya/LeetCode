9837cc2714fbbe4a29eb1a4c35e29e08495722b9
lemoniskartikeya <kaaartikeyawork@gmail.com>
2026-08-22 20:14:38 +0530

    Time: 4 ms (83.13%) | Memory: 46.2 MB (47.74%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        return Arrays.equals(ch1,ch2);
+
+    }
+}
