352af5971febb8fac0ff18e3f948776a3d41d446
kokolemon <kaaartikeyawork@gmail.com>
2026-09-17 19:28:19 +0530

    Time: 8 ms (16.25%) | Memory: 48.4 MB (5.48%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i =0 ; i<nums.length;i++){
+            if(i!=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+
+    }
+}
